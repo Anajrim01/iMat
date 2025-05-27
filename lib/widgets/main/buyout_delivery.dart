881bb45341e6540c581a11ca7eb69dd243d1e88f
BoLaker <pontus@hemert.se>
2025-05-27 19:52:54 +0200

    leveransknapp

--- lib/widgets/main/buyout_delivery.dart
+++ lib/widgets/main/buyout_delivery.dart
@@ -1,8 +1,10 @@
+import 'package:imat_app/widgets/main/time_popup.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:imat_app/app_theme.dart';
 import 'package:imat_app/model/imat_data_handler.dart';
 import 'package:imat_app/widgets/main/delivery_popup.dart';
+
 
 class BuyoutDelivery extends StatefulWidget {
   const BuyoutDelivery({super.key});
@@ -11,16 +13,16 @@ class BuyoutDelivery extends StatefulWidget {
   State<BuyoutDelivery> createState() => BuyoutDeliveryState();
 }
 
-class BuyoutDeliveryState extends State<BuyoutDelivery>{
+class BuyoutDeliveryState extends State<BuyoutDelivery> {
   bool adress_vald = false; // Flytta till klassnivå
-  bool Tid_vald = false;    // Flytta till klassnivå
+  String? selectedTime;     // Sparar vald tid + datum
 
   Future<void> showDeliveryPopup() async {
     final result = await showDialog(
       context: context,
       builder: (context) => const DeliveryPopup(),
     );
-    
+
     if (result != null) {
       setState(() {
         adress_vald = result; // Antar att popupen returnerar ett bool-värde
@@ -28,28 +30,40 @@ class BuyoutDeliveryState extends State<BuyoutDelivery>{
     }
   }
 
+  Future<void> showTimePopup() async {
+    final result = await showDialog<String>(
+      context: context,
+      builder: (context) => const TimePopup(),
+    );
+
+    if (result != null) {
+      setState(() {
+        selectedTime = result; // Sparar vald tid + datum
+      });
+    }
+  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final handler = context.watch<ImatDataHandler>();
     return Center(
       child: Container(
         width: 950,
         height: 430,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
+          borderRadius: BorderRadius.circular(AppTheme.borderRadius),
           boxShadow: [
             BoxShadow(
-              color: Colors.grey.withValues(alpha: 0.2),
+              color: Colors.grey.withAlpha(51), 
               spreadRadius: 1,
               blurRadius: 3,
               offset: const Offset(0, 2),
-          )
+            )
           ],
           color: Colors.white,
         ),
-        child:Column(
-          children: [
-            Padding(
+        child: Column(children: [
+          Padding(
             padding: const EdgeInsets.fromLTRB(80, 80, 0, 0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
@@ -58,187 +72,172 @@ class BuyoutDeliveryState extends State<BuyoutDelivery>{
                 Padding(
                   padding: const EdgeInsets.only(top: 9),
                   child: Icon(
-                  Icons.house,
-                  size: 40,
+                    Icons.house,
+                    size: 40,
+                    color: AppTheme.colorScheme.primary,
                   ),
                 ),
                 Text(
                   "Adress",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.black,
-                    ),
-                  ),
+                  style: AppTheme.textTheme.displayMedium,
+                ),
                 Padding(
                   padding: EdgeInsets.fromLTRB(280, 9, 0, 0),
-                  child: Icon(
-                    Icons.access_time_outlined,
-                    size: 40
-                  )
+                  child: Icon(Icons.access_time_outlined, size: 40, color: AppTheme.colorScheme.primary),
                 ),
                 Text(
                   "Leveranstid",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.black,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(80,20,0,0),
-                  child: Container(
-                    width: 350,
-                    height: 120,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withValues(alpha: 0.2),
-                          spreadRadius: 2,
-                          blurRadius: 3,
-                          offset: const Offset(0, 4),
-                        )
-                      ],
-                    ),
-                    child: Row(
-                      children: [
-                        if(!adress_vald)
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8),
-                          child: Text(
-                            "Ingen adress vald",
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ), 
-                            )
-                          )
-                        else
-                          Text(
-                            "Adress är vald",
-                             style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.normal,
-                              color: Colors.black,
-                            ), 
-                            ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 40),
-                        
-                        child: ElevatedButton(
-                          onPressed: (){
-                            showDeliveryPopup();
-                          }, 
-                          style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey[200],
-                          foregroundColor: Colors.black,
-                          fixedSize: Size(140, 35),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(AppTheme.borderRadius),
-                            ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                            horizontal: 0,
-                            vertical: 0,
-                            ),
-                          child: Text(
-                            'Välj adress',
-                            style: TextStyle(
-                              fontSize: 18
-                            ),
-                            ),
-                          ),
-                        )
-                        )
-                      ],
-                    )
-                  )
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(100,20,0,0),
-                  child: Container(
-                    width: 350,
-                    height: 120,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withValues(alpha: 0.2),
-                          spreadRadius: 2,
-                          blurRadius: 3,
-                          offset: const Offset(0, 4),
-                        )
-                      ],
-                    ),
-                    child: Row(
-                      children: [
-                        if(!Tid_vald)
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8),
-                          child: Text(
-                            "Ingen tid vald",
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ), 
-                            )
-                          )
-                        else
-                          Text(
-                            "Tid är vald",
-                             style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.normal,
-                              color: Colors.black,
-                            ), 
-                            ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 75),
-                        
-                        child: ElevatedButton(
-                          onPressed: (){}, 
-                          style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey[200],
-                          foregroundColor: Colors.black,
-                          fixedSize: Size(140, 35),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(AppTheme.borderRadius),
-                            ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                            horizontal: 0,
-                            vertical: 0,
-                            ),
-                          child: Text(
-                            'Välj tid',
-                            style: TextStyle(
-                              fontSize: 18
-                            ),
-                            ),
-                          ),
-                        )
-                        )
-                      ],
-                    )
-                  )
+                  style: AppTheme.textTheme.displayMedium,
                 )
               ],
-            )
-          ]
-        )
-      )
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(80, 20, 0, 0),
+                child: Container(
+                  width: 350,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(AppTheme.borderRadius),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withAlpha(51),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: const Offset(0, 4),
+                      )
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      if (!adress_vald)
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8),
+                          child: Text(
+                            "Ingen adress vald",
+                            style: AppTheme.textTheme.headlineSmall?.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                        )
+                      else
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8),
+                          child: Text(
+                            "Adress är vald",
+                            style: AppTheme.textTheme.headlineSmall?.copyWith(
+                              fontWeight: FontWeight.normal,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            showDeliveryPopup();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.grey[200],
+                            foregroundColor: Colors.black,
+                            fixedSize: const Size(140, 35),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(AppTheme.borderRadius),
+                            ),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                            child: Text(
+                              'Välj adress',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(100, 20, 0, 0),
+                child: Container(
+                  width: 350,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(AppTheme.borderRadius),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withAlpha(51),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: const Offset(0, 4),
+                      )
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      if (selectedTime == null)
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8),
+                          child: Text(
+                            "Ingen tid vald",
+                            style: AppTheme.textTheme.headlineSmall?.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                        )
+                      else
+                        Flexible(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: Text(
+                              "Vald tid:\n$selectedTime",
+                              style: AppTheme.textTheme.headlineSmall?.copyWith(
+                                color: AppTheme.colorScheme.primary,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 75),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            showTimePopup();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.grey[200],
+                            foregroundColor: Colors.black,
+                            fixedSize: const Size(140, 35),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(AppTheme.borderRadius),
+                            ),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                            child: Text(
+                              'Välj tid',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
+        ]),
+      ),
     );
-  } 
-} 
+  }
+}
---
+++ lib/widgets/main/time_popup.dart
@@ -0,0 +1,263 @@
+import 'package:flutter/material.dart';
+import 'package:imat_app/app_theme.dart';
+
+class TimePopup extends StatefulWidget {
+  const TimePopup({super.key});
+
+  @override
+  State<TimePopup> createState() => _TimePopupState();
+}
+
+class _TimePopupState extends State<TimePopup> {
+  int selectedDayIndex = -1;
+  int selectedTimeIndex = -1;
+  int hoveredDayIndex = -1;
+  int hoveredTimeIndex = -1;
+
+  final List<Map<String, String>> days = [
+    {"day": "Torsdag", "date": "8 maj"},
+    {"day": "Fredag", "date": "9 maj"},
+    {"day": "Lördag", "date": "10 maj"},
+    {"day": "Söndag", "date": "11 maj"},
+  ];
+
+  final List<String> times = [
+    "08:00 - 10:00",
+    "10:00 - 12:00",
+    "12:00 - 14:00",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = AppTheme.colorScheme;
+    final textTheme = AppTheme.textTheme;
+
+    return Dialog(
+      insetPadding: const EdgeInsets.all(AppTheme.paddingLarge),
+      backgroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(AppTheme.borderRadiusLarge),
+      ),
+      child: SizedBox(
+        width: 800,
+        height: 600,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            
+            Padding(
+              padding: const EdgeInsets.all(AppTheme.paddingMedium),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.close),
+                    color: Colors.black54,
+                    iconSize: 28,
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(
+                left: AppTheme.paddingLarge,
+                right: AppTheme.paddingLarge,
+                bottom: AppTheme.paddingMedium,
+              ),
+              child: Text(
+                "Tid för leverans",
+                style: textTheme.displayLarge,
+              ),
+            ),
+
+            
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: AppTheme.paddingMedium),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: List.generate(days.length, (index) {
+                  return _dayCard(
+                    index,
+                    days[index]['day']!,
+                    days[index]['date']!,
+                    index == selectedDayIndex,
+                    () {
+                      setState(() {
+                        selectedDayIndex = index;
+                      });
+                    },
+                    textTheme,
+                    colorScheme,
+                  );
+                }),
+              ),
+            ),
+
+            const SizedBox(height: AppTheme.paddingMedium),
+
+            
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(horizontal: AppTheme.paddingMediumSmall),
+                itemCount: times.length,
+                itemBuilder: (context, index) {
+                  final isSelected = selectedTimeIndex == index;
+                  final isHovered = hoveredTimeIndex == index;
+
+                  return MouseRegion(
+                    onEnter: (_) => setState(() => hoveredTimeIndex = index),
+                    onExit: (_) => setState(() => hoveredTimeIndex = -1),
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          selectedTimeIndex = index;
+                        });
+                      },
+                      borderRadius: BorderRadius.circular(AppTheme.borderRadius),
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(vertical: AppTheme.paddingTiny),
+                        padding: const EdgeInsets.all(AppTheme.paddingSmall),
+                        decoration: BoxDecoration(
+                          color: isSelected
+                              ? colorScheme.primary
+                              : (isHovered ? colorScheme.primary.withOpacity(0.1) : Colors.white),
+                          border: Border.all(
+                            color: isSelected ? colorScheme.primary : Colors.grey.shade300,
+                          ),
+                          borderRadius: BorderRadius.circular(AppTheme.borderRadius),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              times[index],
+                              style: textTheme.bodyLarge!.copyWith(
+                                color: isSelected ? Colors.white : Colors.black,
+                              ),
+                            ),
+                            if (isSelected)
+                              const Padding(
+                                padding: EdgeInsets.only(left: 8.0),
+                                child: Icon(Icons.check, color: Colors.white),
+                              ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            const SizedBox(height: AppTheme.paddingMedium),
+
+            
+            Padding(
+              padding: const EdgeInsets.only(
+                bottom: AppTheme.paddingLarge,
+                left: AppTheme.paddingLarge,
+                right: AppTheme.paddingLarge,
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: selectedDayIndex != -1 && selectedTimeIndex != -1
+                      ? () {
+                          final selectedDay = days[selectedDayIndex];
+                          final selectedTime = times[selectedTimeIndex];
+                          final result =
+                              "${selectedDay['day']} ${selectedDay['date']}, $selectedTime";
+                          Navigator.of(context).pop(result);
+                        }
+                      : null,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: selectedDayIndex != -1 && selectedTimeIndex != -1
+                        ? colorScheme.primary
+                        : Colors.grey,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: AppTheme.paddingMedium,
+                    ),
+                    textStyle: textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(AppTheme.borderRadius),
+                    ),
+                  ),
+                  child: const Text("Klar"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  Widget _dayCard(
+    int index,
+    String title,
+    String date,
+    bool isSelected,
+    VoidCallback onTap,
+    TextTheme textTheme,
+    ColorScheme colorScheme,
+  ) {
+    final isHovered = hoveredDayIndex == index;
+
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      onEnter: (_) => setState(() => hoveredDayIndex = index),
+      onExit: (_) => setState(() => hoveredDayIndex = -1),
+      child: GestureDetector(
+        onTap: onTap,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          padding: const EdgeInsets.symmetric(
+            horizontal: AppTheme.paddingMedium,
+            vertical: AppTheme.paddingSmall,
+          ),
+          decoration: BoxDecoration(
+            color: isSelected
+                ? colorScheme.primary
+                : isHovered
+                    ? colorScheme.primary.withOpacity(0.1)
+                    : Colors.white,
+            border: Border.all(
+              color: isSelected ? colorScheme.primary : Colors.grey,
+              width: 2,
+            ),
+            borderRadius: BorderRadius.circular(AppTheme.borderRadius),
+          ),
+          child: Column(
+            children: [
+              Text(
+                title,
+                style: textTheme.headlineSmall!.copyWith(
+                  color: isSelected ? Colors.white : Colors.black,
+                ),
+              ),
+              Text(
+                date,
+                style: textTheme.bodySmall!.copyWith(
+                  color: isSelected ? Colors.white : Colors.black54,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
